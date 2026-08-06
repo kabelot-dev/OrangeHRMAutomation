7f3edb0d1598a7b5d7b16791e5db6b05213d8628
loadevent <loadevent.academy@gmail.com>
2026-08-07 00:00:04 +0200

    Login POM with valid and invalid login

--- HRM-Automation/HRM-Automation/Pages/LoginPage.cs
+++ HRM-Automation/HRM-Automation/Pages/LoginPage.cs
@@ -1,10 +1,82 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace HRM_Automation.Pages
 {
-    internal class LoginPage
+    internal class LoginPage : BasePage
     {
+ 
+        private IWebElement TxtUsername => Driver.FindElement(By.CssSelector("input[placeholder='Username']"));
+        private IWebElement TxtPassword => Driver.FindElement(By.CssSelector("input[placeholder='Password']"));
+        private IWebElement BtnLogin => Driver.FindElement(By.CssSelector("button[type='submit']"));
+        private IWebElement LinkForgotPass => Driver.FindElement(By.CssSelector(".oxd-text.oxd-text--p.orangehrm-login-forgot-header"));
+
+
+
+
+        public LoginPage(IWebDriver myDriver) : base(myDriver)  
+        {
+            WaitForLoginPageToLoad();
+        }
+
+        public void Login(string username, string password)
+        {
+            TxtUsername.SendKeys(username); 
+            TxtPassword.SendKeys(password);
+            BtnLogin.Click();
+        }
+
+        public string GetErrorMessage()
+        {
+
+            WebElement lblErrorMsg = WaitForElementToBeVisible(".oxd-text.oxd-text--p.oxd-alert-content-text");
+
+            return lblErrorMsg.Text;
+        }
+
+        public void WaitForLoginPageToLoad()
+        {
+            WebDriverWait wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(15));
+            wait.Until(driver => TxtUsername.Displayed && TxtPassword.Displayed && BtnLogin.Displayed);
+        }
+
+        public string OpenForgotPasswordPage()
+        {
+            LinkForgotPass.Click();
+
+            return Driver.Url;
+        }
+
+        public WebElement WaitForElementToBeVisible(string locator)
+        {
+            WebDriverWait wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(10))
+            {
+                PollingInterval = TimeSpan.FromMilliseconds(300),
+            };
+
+            wait.IgnoreExceptionTypes(typeof(NoSuchElementException));
+
+            WebElement element = (WebElement)wait.Until(d =>
+            {
+                return d.FindElement(By.CssSelector(locator));
+            });
+
+            return element;
+
+        }
+
+        public string GetRequiredFieldUsernameErrorMessage()
+        {
+            return WaitForElementToBeVisible("body > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(3) > form:nth-child(2) > div:nth-child(2) > div:nth-child(1) > span:nth-child(3)").Text;
+        }
+
+        public string GetRequiredFieldPasswordErrorMessage()
+        {
+            return WaitForElementToBeVisible("body > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(3) > form:nth-child(2) > div:nth-child(3) > div:nth-child(1) > span:nth-child(3)").Text;
+        }
+
     }
 }
---
+++ HRM-Automation/HRM-Automation/Tests/LoginTest.cs
@@ -0,0 +1,85 @@
+﻿using HRM_Automation.Pages;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HRM_Automation.Tests
+{
+    [TestFixture]
+    internal class LoginTest : BaseTest
+    {
+        [SetUp]
+        public void init() { 
+
+        setup("chrome", "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        }
+
+        [TestCase("Admin", "admin123", "dashboard", TestName = "Valid Login Test",
+            Description = "Verify that user can login with valid credentials",
+            Author = "Kabelo Tlhape")]
+        public void LoginTestWithParameters(string username, string password, string expectedText)
+        {
+            Login_Page?.Login(username, password);
+            bool expected = Driver.Url.Contains(expectedText);
+            Assert.That(expected, $"Url should contain '{expectedText}'");
+        }
+
+        [Test]
+        [Description("Verify login with invalid password")]
+        public void InvalidPasswordLoginTest()
+        {
+            string expectedErrorMessage = "Invalid credentials";
+            Login_Page?.Login("Admin", "Password123");
+            string? actualErrorMessage = Login_Page?.GetErrorMessage();
+            Assert.That(expectedErrorMessage, Is.EqualTo(actualErrorMessage), "Error message should match expected value");
+        }
+
+        [Test]
+        [Description("Verify login using an invalid username")]
+        public void InvalidUsernameLoginTest()
+        {
+            string expectedErrorMessage = "Invalid credentials";
+            Login_Page?.Login("TestUser", "admin123");
+            string? actualErrorMessage = Login_Page?.GetErrorMessage();
+            Assert.That(expectedErrorMessage, Is.EqualTo(actualErrorMessage), "Error message should match expected value");
+        }
+
+        [TestCase("", "admin123", "Required", TestName = "Empty Username Test",
+            Description = "Verify that user cannot login with empty username",
+            Author = "Kabelo Tlhape")]
+        public void EmptyUsernameLoginTest(string username, string password, string expectedErrorMessage)
+        {
+            Login_Page?.Login(username, password);
+            string? actualErrorMessage = Login_Page?.GetRequiredFieldUsernameErrorMessage();
+            Assert.That(expectedErrorMessage, Is.EqualTo(actualErrorMessage), "Error message should match expected value");
+        }
+
+        [TestCase("Admin", "", "Required", TestName = "Empty Password Test",
+        Description = "Verify that user cannot login with empty password",
+        Author = "Kabelo Tlhape")]
+        public void EmptyPasswordLoginTest(string username, string password, string expectedErrorMessage)
+        {
+            Login_Page?.Login(username, password);
+            string? actualErrorMessage = Login_Page?.GetRequiredFieldPasswordErrorMessage();
+            Assert.That(expectedErrorMessage, Is.EqualTo(actualErrorMessage), "Error message should match expected value");
+        }
+
+        [Test(Description = "Verify Forgot Password navigation", Author = "Kabelo Tlhape")]
+        public void ForgotPasswordLinkTest()
+        {
+            string expectedUrl = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/requestPasswordResetCode";
+            string? actualUrl = Login_Page?.OpenForgotPasswordPage();
+            Assert.That(expectedUrl, Is.EqualTo(actualUrl), "Forgot Password page URL should match expected value");
+        }
+
+       
+
+
+
+
+
+    }
+}
